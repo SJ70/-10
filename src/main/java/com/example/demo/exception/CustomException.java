9dc3ef37bfe910f9442609b267d868c582612162
SJ70 <seojae70@gmail.com>
2024-04-16 17:22:29 +0900

    feat: CustomException 예외 클래스 작성

---
+++ src/main/java/com/example/demo/exception/CustomException.java
@@ -0,0 +1,26 @@
+package com.example.demo.exception;
+
+import com.example.demo.response.Code;
+
+public class CustomException extends RuntimeException {
+
+    private Code code;
+    private String message;
+
+    public CustomException(Code code) {
+        this.code = code;
+        this.message = code.name();
+    }
+
+    public CustomException(Code code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+
+}
